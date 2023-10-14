b6b18053b9d1f5cdf9288929a4e48efdab20ac02
TrongDat296 <115761014+TrongDat296@users.noreply.github.com>
2023-10-14 12:16:20 +0700

    first commit

---
+++ App/my-app/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import login from "./src/features/login";
+import home from "./src/features/home";
+import problem_report from "./src/features/problem_report";
+import SendSupport from "./src/features/SendSupport";
+import support from "./src/features/detailSupport";
+
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={login} />
+        <Stack.Screen name="Home" component={home} />
+        <Stack.Screen name="Problem_Report" component={problem_report} />
+        <Stack.Screen name="Send_Support" component={SendSupport} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
